0b4def027cea7a70bbf832ae042407895776f2ef
Fambegbe Olamileke <olamileke.f@gmail.com>
2024-06-09 17:36:34 +0100

    feat: working on email

--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module bayitt/agape
 
 go 1.22.2
 
-require github.com/gin-gonic/gin v1.10.0
+require (
+	github.com/gin-gonic/gin v1.10.0
+	github.com/joho/godotenv v1.5.1
+	github.com/resend/resend-go/v2 v2.6.0
+)
 
 require (
 	github.com/bytedance/sonic v1.11.6 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main;
 import (
+	"github.com/joho/godotenv"
+	"github.com/resend/resend-go/v2"
 	"encoding/json"
 	"os"
 	"path/filepath"
 	"fmt"
+	"time"
 )
 
 
@@ -16,8 +19,16 @@ type TranslationData struct {
 	Translations []Translation `json:"translations"`
 }
 
+type Record struct {
+	Index  		int  		`json:"index"`
+	Timestamp   *time.Time  `json:"timestamp"`
+	Language	*string		`json:"language"` 
+}
+
 
 func main() {
+	godotenv.Load();
+
 	directory, _ := os.Getwd();
 	filePath := filepath.Join(directory, "translations.json");
 	fileContents, _ := os.ReadFile(filePath);
