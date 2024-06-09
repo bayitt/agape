678abe23bd06a282bffcf075cc754860b144fd61
Fambegbe Olamileke <olamileke.f@gmail.com>
2024-06-09 17:09:25 +0100

    feat: started working on app

--- main.go
+++ main.go
@@ -1,4 +1,29 @@
 package main;
 import (
-	"github.com/gin-gonic/gin"
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"fmt"
 )
+
+
+type Translation struct {
+	Language string   `json:"language"` 
+	Text	 string	  `json:"text"`
+}
+
+type TranslationData struct {
+	Translations []Translation `json:"translations"`
+}
+
+
+func main() {
+	directory, _ := os.Getwd();
+	filePath := filepath.Join(directory, "translations.json");
+	fileContents, _ := os.ReadFile(filePath);
+
+	var translationData *TranslationData;
+	json.Unmarshal(fileContents, &translationData);
+
+	fmt.Println(translationData.Translations[12]);
+}
